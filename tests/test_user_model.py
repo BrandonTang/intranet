84a35ee0477180e27457c9ab8e6dc6a6ad99862f
Brandon Tang <blt264@nyu.edu>
2015-06-05 15:44:54 -0400

    Added Features

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,6 +1,6 @@
 import unittest
 from app import create_app, db
-from app.models import User
+from app.models import User, AnonymousUser, Role, Permission
 
 
 class UserModelTestCase(unittest.TestCase):
@@ -33,3 +33,11 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password='cat')
         u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_roles_and_permissions(self):
+        Role.insert_roles()
+        u = User(email='tangbrandon1@gmail.com', password='password')
+        self.assertTrue(u.can(Permission.WRITE_ARTICLES))
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
