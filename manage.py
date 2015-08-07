ee9357488b7485c86b33707f45ffc94f4b0e3e38
Brandon Tang <blt264@nyu.edu>
2015-08-07 17:17:57 -0400

    Updated manage.py; Moved manager down 4 lines;

--- manage.py
+++ manage.py
@@ -6,12 +6,13 @@ from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.sqlalchemy import SQLAlchemy
 
-manager = Manager(app)
-migrate = Migrate(app, db)
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
+manager = Manager(app)
+migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, Post=Post, Tag=Tag, PostTag=PostTag, Comment=Comment)
