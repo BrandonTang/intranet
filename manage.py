60a93d34de035cbc238918a738d5611cbdb057b5
Brandon Tang <blt264@nyu.edu>
2015-08-20 09:58:00 -0400

    Edited additions to manage.py

--- manage.py
+++ manage.py
@@ -10,10 +10,10 @@ from flask.ext.sqlalchemy import SQLAlchemy
 # app = Flask(__name__)
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['ADMIN1'] = os.environ['ADMIN1']
-app.config['ADMIN2'] = os.environ['ADMIN2']
-app.config['DIRECTOR1'] = os.environ['DIRECTOR1']
-app.config['DIRECTOR2'] = os.environ['DIRECTOR2']
+app.config['ADMIN1'] = 'tangbrandon1@gmail.com'
+app.config['ADMIN2'] = 'tangbranodn2@gmail.com'
+app.config['DIRECTOR1'] = 'brandontang1@gmail.com'
+app.config['DIRECTOR2'] = 'brandontang2@gmail.com'
 db = SQLAlchemy(app)
 manager = Manager(app)
 migrate = Migrate(app, db)
